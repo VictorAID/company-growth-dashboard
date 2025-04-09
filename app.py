f905a406f3b89eb418469eee6831313a31774315
VictorAID <75183450+VictorAID@users.noreply.github.com>
2025-04-09 08:36:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -75,6 +75,5 @@ app.layout = html.Div(style={'backgroundColor': colors['background'], 'padding':
     )
 ])
 
-# Run the app
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run_server(host="0.0.0.0", port=8080, debug=True)
